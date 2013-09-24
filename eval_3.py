2c07eb61eee938e0786cdf4dd9adce6916de2d9b
mahdi <m.mazaheri@outlook.com>
2013-09-24 10:02:07 +0330

    eval_3 corrected.

--- eval_3.py
+++ eval_3.py
@@ -28,23 +28,25 @@ if __name__ == '__main__':
     thresholds = np.arange(0.3, 1.05, 0.05)
     ovMax = np.c_[thresholds, np.zeros(np.size(thresholds)), np.zeros(np.size(thresholds))]
 
-    g_check = np.zeros(g_frame, int)
-    c_check = np.zeros(c_frame, int)
+    g_check = np.zeros(np.size(g_frame), int)
+    c_check = np.zeros(np.size(c_frame), int)
 
     for thresh_ind in np.arange(np.size(thresholds)):
         for c_ind in np.arange(np.size(c_frame)):
-            for g_ind in np.where(g_frame == c_frame[c_ind]):
+            # print 'where ',np.where(g_frame == c_frame[c_ind])[0]
+            for g_ind in np.where(g_frame == c_frame[c_ind])[0]:
+                # print g_check[g_ind], c_check[c_ind]
                 if g_check[g_ind] == 0 and c_check[c_ind] == 0:
                     common_area = np.array([max(g_x1[g_ind], c_x1[c_ind]),
                                             max(g_y1[g_ind], c_y1[c_ind]),
                                             min(g_x2[g_ind], c_x2[c_ind]),
                                             min(g_y2[g_ind], c_y2[c_ind])])
-                    common_w = common_area(2) - common_area(0) + 1
-                    common_h = common_area(3) - common_area(1) + 1
+                    common_w = common_area[2] - common_area[0] + 1
+                    common_h = common_area[3] - common_area[1] + 1
 
                     if common_w > 0 and common_h > 0:
-                        u_area =    (g_x2[g_ind] - g_x1[g_ind]) * (g_y2[g_ind] - g_y1[g_ind])
-                                +   (c_x2[c_ind] - c_x1[c_ind]) * (c_y2[c_ind] - c_y1[c_ind])
+                        u_area =    (g_x2[g_ind] - g_x1[g_ind]) * (g_y2[g_ind] - g_y1[g_ind]) \
+                                +   (c_x2[c_ind] - c_x1[c_ind]) * (c_y2[c_ind] - c_y1[c_ind]) \
                                 -   common_w * common_h
 
                         ov = (common_w * common_h) / u_area
@@ -58,4 +60,4 @@ if __name__ == '__main__':
         ovMax[thresh_ind, 1] = tp / (fp + tp)
         ovMax[thresh_ind, 2] = tp / np.size(g_frame)
     s_tmp = candidate.split('.')
-    np.savetxt(s_tmp[0]+'_evals'+s_tmp[1], ovMax, delimiter=',')
+    np.savetxt(s_tmp[0]+'_evals'+s_tmp[1], ovMax, fmt='%1.2f,%i,%i', delimiter=',')
